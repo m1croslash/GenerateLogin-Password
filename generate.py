c0b89e321fcd814814cf5e23c5a1e2a8fa4397e7
microslash <elinalazarevamilana@gmail.com>
2026-01-17 18:50:23 +0900

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,6 +1,6 @@
 import random, string
 
-login = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
-password = ''.join(random.choices(string.ascii_letters  + string.digits + '!@#$%', k=16))
+login = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8)) # k = Character count
+password = ''.join(random.choices(string.ascii_letters  + string.digits + '!@#$%', k=16)) # k = Character count too
 
-print(f'Login: {login}\nPassword: {password}')
+print(f'Login: {login}\nPassword: {password}') # Console output
